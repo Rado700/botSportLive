1ff232708785708e036fa1c8ec7213b29533fe08
Radik <97843156+Rado700@users.noreply.github.com>
2025-03-24 21:31:30 +0300

    Fix: Dockerfile

--- src/main/java/org/example/ApiService.java
+++ src/main/java/org/example/ApiService.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 
 public class ApiService {
 
-    static String API_URL = "http://localhost:8080";
+    static String API_URL = "https://sportliveapp.ru";
 
     public static Object sendGetRequest(String urlStr) throws Exception {
         URL url = new URL(API_URL + urlStr);
